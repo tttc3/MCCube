cec99f9671050e3f4f12d38aef008921d9b433d9
tttc3 <97948946+tttc3@users.noreply.github.com>
2023-08-18 14:49:43 +0100

    Update formulae and region tests.

--- tests/test_formulae.py
+++ tests/test_formulae.py
@@ -3,16 +3,29 @@ import itertools
 import chex
 import sympy
 from absl.testing import absltest, parameterized
+import numpy as np
 
 from mccube.formulae import Hadamard, StroudSecrest63_31, StroudSecrest63_32
 from mccube.regions import GaussianIntegrationRegion
 
+# Test Constants
+DELTA_RESOLUTION = 1e-4  # based on JAX internal tolerances.
+
 # Convert Numerical weight functions into sympy weight functions for each region.
 _numpy_to_sympy_weights = {
-    GaussianIntegrationRegion: lambda syms: sympy.exp(sum([-(x**2) for x in syms]))
+    GaussianIntegrationRegion: lambda x_mat: sympy.exp(-x_mat.T * x_mat)
 }
 
-DELTA_RESOLUTION = 1e-4  # np.finfo(np.float32).resolution
+
+# Generate kwargs for creating affine transformed instances of integration regions.
+def _gaussian_random_kwargs(rng, dimension):
+    mean = rng.uniform(size=dimension)
+    random_matrix = rng.uniform(size=(dimension, dimension))
+    covariance = random_matrix @ random_matrix.T  # Ensures PSD.
+    return dict(mean=mean, covariance=covariance)
+
+
+_affine_region_kwargs = {GaussianIntegrationRegion: _gaussian_random_kwargs}
 
 
 class FormulaTest(chex.TestCase):
@@ -20,6 +33,11 @@ class FormulaTest(chex.TestCase):
     Ensure that cubature formulae can be instantiated correctly, have appropriate
     attributes set, and correctly integrate polynomials of the appropriate degree.
     """
+
+    def setUp(self):
+        self.n_affine = 5
+        self.rng = np.random.default_rng(1234)
+        return super().setUp()
 
     @parameterized.named_parameters(
         ("01D", 1),
@@ -65,64 +83,81 @@ class FormulaTest(chex.TestCase):
         """Primary cubature formulae tests.
 
         Implicitly Tests:
-            - Integration Region Volume
-            - Formula coefficients
-            - Formula vector count
-            - Formula vectors
+            - Integration Region Volume.
+            - Formula coefficients.
+            - Formula vector count.
+            - Formula vectors.
 
         Explicitly Tests:
-            - Formula degree
-            - Formula region normalization
+            - Formula degree.
+            - Formula region normalization.
+            - Formula region transformation.
+            - Formula for affine transformations of the region.
         """
         self.assertIsInstance(formula.region, expected_region)
         self.assertEqual(formula.degree, expected_degree)
         self.assertEqual(formula.vector_count, formula.vectors.shape[0])
 
-        # Check that the cubature formula is indeed of the expected degree.
-        monomial_generator = itertools.combinations_with_replacement(
-            range(formula.degree + 1), formula.dimension
-        )
-        exact_monomial_generator = itertools.filterfalse(
-            lambda x: sum(x) > formula.degree, monomial_generator
-        )
-
-        for monomial_degrees in exact_monomial_generator:
-            (
-                volume_integral,
-                integral,
-                unweighted_integrand,
-                symbolic_integrand,
-            ) = self.symbolic_monomial_integrals(formula, monomial_degrees)
-            print(f"Testing cubature for monomial: {symbolic_integrand}")
-            self.assertAlmostEqual(volume_integral, formula.region.volume)
-            # Ideally would test closeness via ULP rather than performing this scaling.
-            # TODO: investigate ULP closeness tests via chex.
-            self.assertAlmostEqual(
-                integral / volume_integral,
-                formula(unweighted_integrand, normalize=False)[0] / volume_integral,
-                delta=DELTA_RESOLUTION,
+        formulae = [formula]
+        affine_formulae = [
+            formula.transform(
+                **_affine_region_kwargs[formula.region.__class__](
+                    self.rng, formula.dimension
+                ),
             )
-            self.assertAlmostEqual(
-                integral / volume_integral,
-                formula(unweighted_integrand, normalize=True)[0],
-                delta=DELTA_RESOLUTION,
-            )
+        ]
+        # Sympy integration is too expensive to perform at dimensions above two.
+        if formula.dimension < 3:
+            formulae += affine_formulae
 
-        # Check the cubature for affine transformations of the region.
-        # Test for standard normal and some random transformations.
-        # transformed_formula = formula.transform()
+        for formula in formulae:
+            # Check that the cubature formula is indeed of the expected degree.
+            monomial_generator = itertools.combinations_with_replacement(
+                range(formula.degree + 1), formula.dimension
+            )
+            exact_monomial_generator = itertools.filterfalse(
+                lambda x: sum(x) > formula.degree, monomial_generator
+            )
+            for monomial_degrees in exact_monomial_generator:
+                (
+                    volume_integral,
+                    integral,
+                    unweighted_integrand,
+                    symbolic_integrand,
+                ) = self.symbolic_monomial_integrals(formula, monomial_degrees)
+                print(f"Testing cubature for monomial: {symbolic_integrand}")
+                # Ideally would test closeness via ULP rather than this scaling.
+                # TODO: investigate ULP closeness tests via chex.
+                self.assertAlmostEqual(
+                    integral / volume_integral,
+                    formula(unweighted_integrand, normalize=False)[0] / volume_integral,
+                    delta=DELTA_RESOLUTION,
+                )
+                self.assertAlmostEqual(
+                    integral / volume_integral,
+                    formula(unweighted_integrand, normalize=True)[0],
+                    delta=DELTA_RESOLUTION,
+                )
 
     def symbolic_monomial_integrals(self, formula, monomial_degrees):
         """
         Generates integral and integrand all d-dimension polynomials of degree not
         exceeding $m$.
         """
-        # Constuct symbolic integrand.
+        # Construct symbolic integrand.
         syms = sympy.symbols([f"x_{i}" for i, _ in enumerate(monomial_degrees)])
+        syms_affine = sympy.Matrix([1, *syms])
+        # Convert floats to nearest rational to improve sympy integration performance.
+        rational_transform = sympy.nsimplify(
+            sympy.Matrix(formula.region.affine_transformation_matrix), rational=True
+        ).inv()
+        transformed_syms = rational_transform * syms_affine
+        transformed_syms = transformed_syms[1:, :]
         monomial = sympy.prod([s**d for s, d in zip(syms, monomial_degrees)])
-        weight = _numpy_to_sympy_weights[formula.region.__class__](syms)
+        weight = _numpy_to_sympy_weights[formula.region.__class__](transformed_syms)[0]
         integrand = monomial * weight
         # Perform symbolic integration.
+        # TODO: region needs to change based on formula.region type.W
         volume = sympy.integrate(weight, *[(x, -sympy.oo, sympy.oo) for x in syms])
         integral = sympy.integrate(integrand, *[(x, -sympy.oo, sympy.oo) for x in syms])
         lambdified_monomial = sympy.lambdify(syms, monomial, modules="jax")
--- tests/test_regions.py
+++ tests/test_regions.py
@@ -1,10 +1,9 @@
 import chex
-
 import numpy as np
-
+import sympy as sp
 from absl.testing import absltest, parameterized
 
-from mccube.regions import _psd_quadratic_transformation, GaussianIntegrationRegion
+from mccube.regions import GaussianIntegrationRegion, _psd_quadratic_transformation
 
 
 class RegionTest(chex.TestCase):
@@ -12,6 +11,31 @@ class RegionTest(chex.TestCase):
     Ensure that cubature regions can be instantiated correctly, have appropriate
     attributes set, and correctly define their affine transformations.
     """
+
+    def setUp(self):
+        # As per example 2 pg 10-12 :cite:p:`stroud1971`, except $T$ is denoted $M$
+        # fmt: off
+        # x as per eq 1.4-10.
+        self.x = sp.Matrix([[1, 1, 0],
+                            [1,-1, 0],
+                            [1, 0, 1],
+                            [1, 0,-1]])
+        self.A = sp.Matrix([[1,0,0],
+                            [0,1,0],
+                            [0,0,1]])
+        # B is as per eq 1.4-11.
+        self.B = sp.Matrix([[ 50,  0,  0],
+                            [-20, 20,-16],
+                            [-20,-16, 20]]) / 9
+        self.M_inv = sp.Matrix([[    1,                 0,                 0],
+                    [sp.Rational(-5,3),  sp.Rational(4,3), sp.Rational(-2,3)],
+                    [sp.Rational(-5,3), sp.Rational(-2,3),  sp.Rational(4,3)]])
+
+        self.A_COV = (2*self.A[1:,1:]).inv()
+        self.B_COV = (2*self.B[1:,1:]).inv()
+        self.A_MEAN = self.A[1:, 0]
+        self.B_MEAN = -self.M_inv.inv()[1:, 1:] @ self.M_inv[1:, 0]
+        return super().setUp()
 
     # fmt: off
     @parameterized.named_parameters(
@@ -21,52 +45,51 @@ class RegionTest(chex.TestCase):
         ("4D (None Mean + Cov)", 4,           None,          None),
     )
     # fmt: on
-    def test_gaussian(self, dimension, mean, covariance):
+    def test_gaussian_init(self, dimension, mean, covariance):
         region = GaussianIntegrationRegion(dimension, mean, covariance)
         self.assertEqual(region.dimension, dimension)
         test_mean = np.zeros(dimension) if mean is None else mean
         test_covariance = np.eye(dimension) / 2 if covariance is None else covariance
+        # Test defaults.
         chex.assert_trees_all_equal(region.mean, test_mean)
         chex.assert_trees_all_equal(region.covariance, test_covariance)
 
-        test_affine = np.eye(dimension + 1)
-        test_affine[1:, 1:] = test_covariance
-        test_affine[1:, 0] = test_mean
-
-        canonical_affine = np.eye(dimension + 1)
-        canonical_affine[1:, 1:] = np.eye(dimension) / 2
-        test_transform = _psd_quadratic_transformation(canonical_affine, test_affine)
-        chex.assert_trees_all_equal(region.affine_transformation_matrix, test_transform)
+    def test_gaussian_affine_transformation(self):
+        """
+        Check that, given a desired mean and covariance for the multivariate gaussian
+        weight, the correct affine_transformation_matrix is generated.
+        """
+        mean = sp.matrix2numpy(self.B_MEAN, dtype=float)
+        cov = sp.matrix2numpy(self.B_COV, dtype=float)
+        region = GaussianIntegrationRegion(2, mean=mean, covariance=cov)
+        chex.assert_trees_all_close(
+            region.affine_transformation_matrix,
+            sp.matrix2numpy(self.M_inv.inv(), dtype=float),
+        )
 
     def test_psd_quadratic_transformation(self):
-        """As per example 2 pg 10-12 :cite:p:`stroud1971`, except $T$ is denoted $M$."""
-        # fmt: off
-        # X as per eq 1.4-10.
-        x = np.array([[1, 1, 0],
-                      [1,-1, 0],
-                      [1, 0, 1],
-                      [1, 0,-1]])
-        A = np.array([[1,0,0],
-                      [0,1,0],
-                      [0,0,1]])
-        # B is as per eq 1.4-11.
-        B = np.array([[ 50,  0,  0],
-                      [-20, 20,-16],
-                      [-20,-16, 20]]) / 9
-        M_inv = np.array([[   1,  0,    0],
-                          [-5/3, 4/3,-2/3],
-                          [-5/3,-2/3, 4/3]])
-        # fmt: on
-        # Check linear
-        M_linear = _psd_quadratic_transformation(A[1:, 1:], B[1:, 1:], affine=False)
+        """
+        Check that affine quadratic transformation matrix is computed correctly.
+        As per example 2 pg 10-12 :cite:p:`stroud1971`, except $T$ is denoted $M$.
+        """
+        A = sp.matrix2numpy(self.A, dtype=float)
+        B = sp.matrix2numpy(self.B, dtype=float)
+        M_inv = sp.matrix2numpy(self.M_inv, dtype=float)
+        M = sp.matrix2numpy(self.M_inv.inv(), dtype=float)
+        # Check linear.
+        M_linear = _psd_quadratic_transformation(A[1:, 1:], B[1:, 1:])
         chex.assert_tree_all_close(B[1:, 1:], M_linear.T @ A[1:, 1:] @ M_linear)
         chex.assert_trees_all_close(M_inv[1:, 1:], M_linear)
-        chex.assert_trees_all_close(M_inv[1:, 1:] @ x[:, 1:].T, M_linear @ x[:, 1:].T)
-
-        # Check affine
-        M_affine = _psd_quadratic_transformation(A, B)
+        # Check affine.
+        M_affine = _psd_quadratic_transformation(A, B, affine=True)
         chex.assert_trees_all_close(M_inv, M_affine)
-        chex.assert_trees_all_close(M_inv @ x.T, M_affine @ x.T)
+        # Check Inverted.
+        M_linear_inv = _psd_quadratic_transformation(
+            A[1:, 1:], B[1:, 1:], inverted=True
+        )
+        chex.assert_tree_all_close(M[1:, 1:], M_linear_inv)
+        M_affine_inv = _psd_quadratic_transformation(A, B, affine=True, inverted=True)
+        chex.assert_tree_all_close(M, M_affine_inv)
 
 
 if __name__ == "__main__":
