20e1f21c955d7081e34b4ca09960c0f3c8ed165b
tttc3 <T.Coxon2@lboro.ac.uk>
2023-11-20 11:52:28 +0000

    Updated cubature region api.

--- mccube/regions.py
+++ mccube/regions.py
@@ -1,93 +1,223 @@
-"""Defines abstract and concrete integration regions."""
+"""
+Defines the integration regions (measure spaces) against which :mod:`mccube.formulae` 
+can be defined.
+"""
 from __future__ import annotations
 
 import abc
+from functools import cached_property
+from typing import ClassVar
 
 import equinox as eqx
 import numpy as np
 from jaxtyping import Array, ArrayLike, Float
 
 
-class AbstractIntegrationRegion(eqx.Module):
-    r"""Abstract base class for all integration regions.
+class AbstractRegion(eqx.Module):
+    r"""Abstract base class for all (weighted) integration regions.
+
+    Integration regions are measure spaces $(\Omega, \mathcal{F}, \mu)$, where
+    $\mathcal{F}$ is the Borel $\sigma$-algebra on the region $\Omega$, and $\mu$ is
+    some suitable Borel (probability) measure that 'weights' the region. It is assumed
+    that the measure $\mu(x) = \mu^\prime(Mx)$, an affine transformation of the
+    canonical measure for the region $\mu^\prime$.
+
 
     Attributes:
         dimension: dimension $d$ of the integration region $\Omega$.
+        normalized: if the measure $\mu$ is a probability measure (normalized to have
+            a volume of one).
     """
-
     dimension: int
+    normalized: bool
 
-    @abc.abstractmethod
-    def weight_function(x: ArrayLike) -> Array:
-        r"""Integration weight function/distribution $w$."""
-        ...
-
-    @abc.abstractproperty
+    @cached_property
     def volume(self) -> float:
-        r"""Volume $V$ of the weighted integration region $\Omega$."""
-        ...
+        r"""Measure $\mu$ of $\Omega$, given by $V=V^\prime\det(M)$."""
+        return self._volume * self.affine_transformation[1]
 
     @abc.abstractproperty
-    def affine_transformation_matrix(self) -> Float[ArrayLike, "d+1 d+1"]:
-        r"""Affine transformation matrix from the canonical region $M$."""
+    def _volume(self) -> float:
+        r"""Measure $\mu^\prime$ of $\Omega$, denoted $V^\prime$."""
+        ...
+
+    @cached_property
+    def affine_transformation(self) -> (Float[Array, "d+1 d+1"], float):
+        r"""A potentially normalized affine transformation M on $\Omega$ and its determinant $\det(M)$.
+
+        Constructs the potentially normalized affine transformation $M$ from the
+        canonical measure for the region $\mu^\prime$, to the potentially normalized
+        (probability) measure $\mu$, given the unnormalized affine transformation
+        $M^\prime$. If :attr:`~AbsractRegion.normalized` is set, then the linear
+        component of $M = cM^\prime$ where $c = (V^\prime \det(M^\prime))^{-1/d}$,
+        otherwise $M = M^\prime$.
+        """
+        matrix, det = self._affine_transformation
+        if self.normalized:
+            # Rescale such that the linear sub-matrix of the affine transformation has a
+            # determinant of 1/self._volume.
+            target_det = 1 / self._volume
+            normalization_factor = (target_det / det) ** (1 / self.dimension)
+            matrix[1:, 1:] *= normalization_factor
+            det = target_det
+        return matrix, det
+
+    @abc.abstractproperty
+    def _affine_transformation(self) -> (Float[Array, "d+1 d+1"], float):
+        r"""An affine transformation $M^\prime$ on $\Omega$ and its determinant $\det(M^\prime)$.
+
+        Constructs the affine transformation $M^\prime$ from the canonical measure for
+        the region $\mu^\prime$ to the unnormalized measure $\mu(x) = \mu^\prime(M^\prime x)$.
+        """
         ...
 
 
-class GaussianIntegrationRegion(AbstractIntegrationRegion):
-    r"""d-dimensional unnormalized gaussian weighted Euclidean integration region.
+class GaussianRegion(AbstractRegion):
+    r"""Euclidean space :math:`\mathbb{R}^d` with (unnormalized) Gaussian measure.
 
-    Notated as $E_n^{r^2}$ in :cite:p:`stroud1971`, this region represents integration
-    over an d-dimensional Euclidean space, weighted by $\exp{-x_1^2 \dots -x_d^2}$.
+    Denoted as $E_n^{r^2}$ in :cite:p:`stroud1971`, this integration region represents
+    the measure space $(\mathbb{R}^d, \mathcal{F}, \mu)$, where $\mu$ is a potentially
+    normalized affine transformation of the unnomalized Gaussian measure, with mean zero
+    and diagonal covariance of one half, given by $\mu^\prime(x_1, \dots, x_d) = \exp(-x_1^2 -\dots-x_d^2)$.
+
+    Note: the canonical measure $\mu^\prime$ is the weight for which the "physicist's"
+    $d$-dimensional Hermite polynomials are orthogonal.
 
     Attributes:
-        dimension: dimension $d$ of the integration region $\Omega$.
-        mean: mean parameter for the Gaussian weight function.
-        covariance: covariance parameter for the Gaussian weight function.
-        affine_transformation_matrix: a matrix specifying an affine transformation of
-            the integration region.
+        dimension: dimension $d$ of the Euclidean space $\mathbb{R}^d$.
+        mean: mean (location) parameter for the $d$-dimensional Gaussian measure $\mu$;
+            the canonical measure $\mu^\prime$ has mean given by the $d$-dimensional
+            zero vector, $\boldsymbol{0}$.
+        covariance: covariance (scale) parameter for the $d$-dimensional Gaussian
+            measure $\mu$; the canonical measure $\mu^\prime$ has covariance given by
+            the $d$-dimensional identity matrix times one half, $0.5*I_d$.
+        normalized: if to normalize the affine transformed measure $\mu^\prime$, such
+            that $\mu$ is a $d$-dimensional Gaussian probability measure.
     """
+    dimension: int
     mean: Float[ArrayLike, " d"]
     covariance: Float[ArrayLike, "d d"]
+    normalized: bool
+    _default_mean: Float[ArrayLike, " d"]
+    _default_cov: Float[ArrayLike, "d d"]
 
     def __init__(
         self,
         dimension: int,
         mean: None | Float[ArrayLike, " d"] = None,
         covariance: None | Float[ArrayLike, "d d"] = None,
+        normalized: bool = False,
     ):
-        r"""Initialise Gaussian integration region with specified mean and covariance.
+        r"""Construct a $d$-dimensional Gaussian region.
+
+        Parametrizes the :attr:`~GaussianRegion.affine_transformation` such that
+        $\mu^\prime$ (the affine transformed measure) is the $d$-dimensional Gaussian
+        measure with the specified mean and covariance.
 
         Args:
-            dimension: dimension $d$ of the integration region $\Omega$.
-            mean: if specified, mean should be a $d$ vector; implicitly zero if None.
+            dimension: dimension $d$ of the Euclidean space $\mathbb{R}^d$.
+            mean: if specified, mean should be a $d$-dimensional vector; implicitly zero
+                if None.
             covariance: if specified, covariance should be a $d \times d$ matrix;
                 implicitly diag(1/2) if None.
+            normalized: if to normalize the Gaussian measure.
         """
-        self.dimension = dimension
-        self.mean = np.zeros(dimension) if mean is None else mean
-        self.covariance = np.eye(dimension) / 2 if covariance is None else covariance
+        self._default_mean = np.zeros(dimension)
+        self._default_cov = np.eye(dimension) / 2
 
-    def weight_function(self, x: ArrayLike) -> Array:
-        return np.exp(-np.sum(x**2, axis=-1))
+        self.dimension = dimension
+        self.mean = self._default_mean if mean is None else mean
+        self.covariance = self._default_cov if covariance is None else covariance
+        self.normalized = normalized
 
     @property
-    def volume(self) -> float:
+    def _volume(self) -> float:
+        r"""Measure $\mu^\prime$ of $\Omega$, given by $V^\prime=\pi^{d/2}$."""
         return np.pi ** (self.dimension / 2)
 
     @property
-    def affine_transformation_matrix(self):
-        default_cov = np.eye(self.dimension) / 2
-        target_cov = self.covariance
+    def _affine_transformation(self) -> (Float[Array, "d d"], float):
+        r"""An affine transformation $M$ on $\Omega$ and its determinant $\det(M)$.
 
-        transform_cov = _psd_quadratic_transformation(default_cov, target_cov)
-        transform_mean = self.mean
+        Computes the appropriate affine transformation to yield an implied measure
+        $\mu^\prime$ with the attribute specified mean and covariance, given the
+        mean and covariance for the canonical measure $\mu$.
+        """
+        transform_cov, det = psd_quadratic_transformation(
+            self._default_cov, self.covariance
+        )
         transform = np.eye(self.dimension + 1)
         transform[1:, 1:] = transform_cov
-        transform[1:, 0] = np.squeeze(transform_mean)
-        return transform
+        transform[1:, 0] = np.squeeze(self.mean)
+        return (transform, det)
 
 
-def _psd_quadratic_transformation(A, B, affine=False, inverted=False):
+class StandardGaussianRegion(GaussianRegion):
+    r"""Euclidean space :math:`\mathbb{R}^d` with normalized Standard Gaussian measure.
+
+    While the :class:`GuassianRegion` defines a measure for which the "physicist's"
+    $d$-dimensional Hermite polynomials are orthogonal, this region defines a measure
+    for which the "probabilist's" $d$-dimensional Hermite polynomials are orthogonal.
+
+    Note: this class is just a specific parameterization of the :class:`GaussianRegion`
+    where the :attr:`~GaussianRegion.mean` is zero and :attr:`~GaussianRegion.covariance`
+    is the $d \times d$ identity matrix, $I_d$.
+    """
+
+    def __init__(self, dimension: int) -> None:
+        r"""Construct a $d$-dimensional Standard Gaussian region.
+
+        Parametrizes the :attr:`~GaussianRegion.affine_transformation` such that
+        $\mu^\prime$ (the affine transformed measure) is the normalized $d$-dimensional
+        Gaussian with :attr:`~GaussianRegion.mean` zero and :attr:`~GaussianRegion.covariance`
+        given by the $d \times d$ identity matrix, $I_d$.
+
+        Args:
+            dimension: dimension $d$ of the Euclidean space $\mathbb{R}^d$.
+        """
+        return super().__init__(
+            dimension,
+            mean=np.zeros(dimension),
+            covariance=np.eye(dimension),
+            normalized=True,
+        )
+
+
+class WienerSpace(AbstractRegion):
+    r"""Classical Wiener space with paths :math:`\omega \in C_0^0([0,T], \mathbb{R}^d)`.
+
+    An infinite-dimensional space of continuous paths (functions) defined over the
+    interval $[0,T]$, starting at zero, and taking values in $\mathbb{R}^d$. Defines a
+    measure space $(C_0^0([0,T], \mathbb{R}^d), \mathcal{F}, \mu)$, where $\mathcal{F}$
+    is the Borel $\sigma$-field and $\mu$ is the standard Wiener measure.
+
+    The canonical measure $\mu^\prime$ is defined for the Wiener space over the unit
+    interval and, for any desired $T > 0$, the measure $\mu = \sqrt{T}\mu^\prime$,
+    thanks to the scaling properties of Wiener space :cite:p:`lyons2004`.
+
+    Attributes:
+        dimension: dimension $d$ of the Euclidean space in which the paths take values.
+        dt: the width of the time-domain $[0, T]$ on which the Wiener space is defined.
+            As the time-domain is defined to start at zero, $dt = T$.
+    """
+    dimension: int
+    dt: float = 1.0
+    normalized: ClassVar[bool] = True
+
+    @property
+    def _volume(self) -> float:
+        return 1.0
+
+    @property
+    def _affine_transformation(self) -> (float, float):
+        dt = self.dt
+        t_affine = np.array([[1, 0], [0, dt**0.5]])
+        return t_affine, 1.0
+
+
+def psd_quadratic_transformation(
+    A: Float[Array, "d d"], B: Float[Array, "d d"], affine=False, inverted=False
+) -> (Float[Array, "d d"], float):
     r"""Compute transformation matrix from A to B.
 
     Args:
@@ -96,10 +226,10 @@ def _psd_quadratic_transformation(A, B, affine=False, inverted=False):
         B: the target transformation $B = M^T A M$. The linear component must be
             positive definite.
         affine: indicates if to treat A and B as affine transformation matrices.
-        inverted: indicates if to return $M^-1$. Avoids repeating expensive inversions.
+        inverted: indicates if to return $M^{-1}$. Avoids repeating expensive inversions.
 
     Returns:
-        The (affine) transformation matrix $M$.
+        The (affine) transformation matrix $M$ and the determinant $\det(M)$.
     """
 
     if affine:
@@ -109,17 +239,19 @@ def _psd_quadratic_transformation(A, B, affine=False, inverted=False):
         B = B[1:, 1:]
     D_B, P = np.linalg.eigh(B)
     D = np.sqrt(D_B / np.diag(A))
+    det = np.prod(D)  # Matrix cookbook pg6 equation (18)
     M_quadratic = P @ np.diag(D) @ P.T
     M_quadratic_inv = P @ np.diag(1 / D) @ P.T
     if not affine:
         if inverted:
-            return M_quadratic_inv
-        return M_quadratic
+            return M_quadratic_inv, 1 / det
+        return M_quadratic, det
     M_translate = (M_quadratic_inv @ B_translate - A_translate) / 2
     M_affine = np.eye(A.shape[0] + 1)
     if inverted:
+        det = 1 / det
         M_quadratic = M_quadratic_inv
         M_translate = -M_quadratic_inv @ M_translate
     M_affine[1:, 1:] = M_quadratic
     M_affine[1:, 0] = M_translate
-    return M_affine
+    return M_affine, det
--- tests/test_regions.py
+++ tests/test_regions.py
@@ -3,14 +3,18 @@ import numpy as np
 import sympy as sp
 from absl.testing import absltest, parameterized
 
-from mccube.regions import GaussianIntegrationRegion, _psd_quadratic_transformation
+import jax.tree_util as jtu
+
+from mccube.regions import (
+    GaussianRegion,
+    StandardGaussianRegion,
+    WienerSpace,
+    psd_quadratic_transformation,
+)
 
 
-class RegionTest(chex.TestCase):
-    """
-    Ensure that cubature regions can be instantiated correctly, have appropriate
-    attributes set, and correctly define their affine transformations.
-    """
+class GaussianRegionTests(chex.TestCase):
+    """Tests defining the behavior of :class:`GaussianRegion` and :class:`StandardGaussianRegion`."""
 
     def setUp(self):
         # As per example 2 pg 10-12 :cite:p:`stroud1971`, except $T$ is denoted $M$
@@ -39,32 +43,89 @@ class RegionTest(chex.TestCase):
 
     # fmt: off
     @parameterized.named_parameters(
-        ("1D (Scalar)"         , 1, 3 * np.ones(1), 4 * np.eye(1)),
-        ("2D (None Mean)"      , 2,           None, 2 * np.eye(2)),
-        ("3D (None Cov)"       , 3, 2 * np.ones(3),          None),
-        ("4D (None Mean + Cov)", 4,           None,          None),
+        ("1D (Scalar)"                    , 1, 3 * np.ones(1), 4 * np.eye(1), False),
+        ("2D (None Mean)"                 , 2,           None, 2 * np.eye(2), False),
+        ("3D (None Cov)"                  , 3, 2 * np.ones(3),          None, False),
+        ("4D (None Mean + Cov)"           , 4,           None,          None, False),
+        ("1D (Normalized Scalar)"         , 1, 3 * np.ones(1), 4 * np.eye(1),  True),
+        ("2D (Normalized None Mean)"      , 2,           None, 2 * np.eye(2),  True),
+        ("3D (Normalized None Cov)"       , 3, 2 * np.ones(3),          None,  True),
+        ("4D (Normalized None Mean + Cov)", 4,           None,          None,  True),
     )
     # fmt: on
-    def test_gaussian_init(self, dimension, mean, covariance):
-        region = GaussianIntegrationRegion(dimension, mean, covariance)
+    def test_gaussian_init(self, dimension, mean, covariance, normalized):
+        """
+        Check that attributes are initialized properly:
+            - dimension,
+            - normalized (expected default=False),
+            - mean (expected default=np.zeros(dimension)),
+            - covariance (expected default=np.eye(dimension) / 2).
+
+        Check that properties give the expected values:
+            - volume (expected 1.0 when normalized=True; else expected det(M) pi^(d/2),
+            as per :cite:`stroud1971` (pg 222, Section 7.9)).
+
+        Assumptions:
+            - :method:`test_gaussian_affine_transformation` passes.
+        """
+        region = GaussianRegion(dimension, mean, covariance, normalized)
+
+        ## Check attributes.
         self.assertEqual(region.dimension, dimension)
+        # Check defaults
+        test_normalized = False if normalized is None else normalized
         test_mean = np.zeros(dimension) if mean is None else mean
         test_covariance = np.eye(dimension) / 2 if covariance is None else covariance
-        # Test defaults.
+        self.assertEqual(region.normalized, test_normalized)
         chex.assert_trees_all_equal(region.mean, test_mean)
         chex.assert_trees_all_equal(region.covariance, test_covariance)
+
+        ## Check properties
+        detM = region.affine_transformation[1]
+        expected_volume = detM * np.pi ** (region.dimension / 2)
+        if region.normalized:
+            self.assertAlmostEqual(expected_volume, 1.0)
+        self.assertAlmostEqual(region.volume, expected_volume)
 
     def test_gaussian_affine_transformation(self):
         """
         Check that, given a desired mean and covariance for the multivariate gaussian
-        weight, the correct affine_transformation_matrix is generated.
+        measure, the correct affine_transformation is generated.
         """
         mean = sp.matrix2numpy(self.B_MEAN, dtype=float)
         cov = sp.matrix2numpy(self.B_COV, dtype=float)
-        region = GaussianIntegrationRegion(2, mean=mean, covariance=cov)
+        region = GaussianRegion(2, mean=mean, covariance=cov)
         chex.assert_trees_all_close(
-            region.affine_transformation_matrix,
+            region.affine_transformation[0],
             sp.matrix2numpy(self.M_inv.inv(), dtype=float),
+        )
+        chex.assert_trees_all_close(
+            region.affine_transformation[1], float(self.M_inv.inv().det())
+        )
+
+    @parameterized.named_parameters(
+        ("1D", 1),
+        ("2D", 2),
+        ("3D", 3),
+        ("4D", 4),
+    )
+    def test_standard_gaussian_region_init(self, dimension):
+        """
+        Check that a StandardGaussianRegion is equivalent to a normalized GaussianRegion
+        with mean zero and diagonal covariance of one.
+
+        Assumptions:
+            - :method:`test_gaussian_init` passes.
+        """
+        standard_region = StandardGaussianRegion(dimension)
+        region = GaussianRegion(
+            dimension,
+            mean=np.zeros(dimension),
+            covariance=np.eye(dimension),
+            normalized=True,
+        )
+        chex.assert_trees_all_equal(
+            jtu.tree_leaves(standard_region), jtu.tree_leaves(region)
         )
 
     def test_psd_quadratic_transformation(self):
@@ -77,19 +138,66 @@ class RegionTest(chex.TestCase):
         M_inv = sp.matrix2numpy(self.M_inv, dtype=float)
         M = sp.matrix2numpy(self.M_inv.inv(), dtype=float)
         # Check linear.
-        M_linear = _psd_quadratic_transformation(A[1:, 1:], B[1:, 1:])
+        M_linear, det_linear = psd_quadratic_transformation(A[1:, 1:], B[1:, 1:])
         chex.assert_trees_all_close(B[1:, 1:], M_linear.T @ A[1:, 1:] @ M_linear)
         chex.assert_trees_all_close(M_inv[1:, 1:], M_linear)
         # Check affine.
-        M_affine = _psd_quadratic_transformation(A, B, affine=True)
+        M_affine, det_affine = psd_quadratic_transformation(A, B, affine=True)
         chex.assert_trees_all_close(M_inv, M_affine)
         # Check Inverted.
-        M_linear_inv = _psd_quadratic_transformation(
+        M_linear_inv, det_linear_inv = psd_quadratic_transformation(
             A[1:, 1:], B[1:, 1:], inverted=True
         )
         chex.assert_trees_all_close(M[1:, 1:], M_linear_inv)
-        M_affine_inv = _psd_quadratic_transformation(A, B, affine=True, inverted=True)
+        M_affine_inv, det_affine_inv = psd_quadratic_transformation(
+            A, B, affine=True, inverted=True
+        )
         chex.assert_trees_all_close(M, M_affine_inv)
+
+
+class WienerRegionTests(chex.TestCase):
+    """Tests defining the behavior of :class:`WienerSpace`."""
+
+    # fmt: off
+    @parameterized.named_parameters(
+        ("1D"          , 1, None),
+        ("2D"          , 2, None),
+        ("3D"          , 3, None),
+        ("4D"          , 4, None),
+        ("1D (dt=001)" , 1, 0.01),
+        ("2D (dt=02)"  , 2,  0.2),
+        ("3D (dt=3)"   , 3,  3.0),
+        ("4D (dt=40)"  , 4, 40.0),
+    )
+    # fmt: on
+    def test_wiener_space_init(self, dimension, dt):
+        """
+        Check that attributes are initialized properly:
+            - dimension,
+            - dt (expected default=1.0),
+            - normalized (expected always True).
+
+        Check that properties give the expected values:
+            - volume (expected always 1.0, by definition of always normalized=True).
+            - affine_transformation (expected scale by M = sqrt(dt) and detM = 1).
+        """
+
+        args = [dimension]
+        test_dt = 1.0
+        if dt is not None:
+            test_dt = dt
+            args.append(dt)
+        region = WienerSpace(*args)
+
+        ## Check attributes
+        self.assertEqual(region.dimension, dimension)
+        self.assertEqual(region.dt, test_dt)
+        self.assertEqual(region.normalized, True)
+
+        ## Check properties
+        self.assertEqual(region.volume, 1.0)
+        test_affine = (np.array([[1, 0], [0, test_dt**0.5]]), 1.0)
+        chex.assert_trees_all_equal(region.affine_transformation, test_affine)
 
 
 if __name__ == "__main__":
